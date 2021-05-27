6f7d79248853bf130d7f8d46a30d06b4069eca79
otmane aatik <aaotmane@gmail.com>
2021-05-27 14:56:40 -0400

    slider update

--- src/components/Player.js
+++ src/components/Player.js
@@ -32,13 +32,22 @@ const Player = ( { currentSong, isPlaying, setIsPlaying }) => {
             Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
         );
     }
+
+    const dragHandler = (e) => {
+        setSongInfo({...songInfo, currentTime: e.target.value});
+        audioRef.current.currentTime = e.target.value;
+    }
     
 
     return(
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input type="range"/>
+                <input type="range" 
+                min={0} 
+                max={songInfo.duration} 
+                value={songInfo.currentTime}
+                onChange={dragHandler}/>
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
@@ -46,7 +55,10 @@ const Player = ( { currentSong, isPlaying, setIsPlaying }) => {
                 <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
-            <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
+            <audio src={currentSong.audio}
+            onTimeUpdate={timeUpdateHandler} 
+            onLoadedMetadata={timeUpdateHandler} 
+            ref={audioRef} ></audio>
         </div>
     )
 }

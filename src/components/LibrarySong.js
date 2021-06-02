99d9e5dbd31f5762c3bdd01e011e46ce4b2d62db
otmane aatik <aaotmane@gmail.com>
2021-06-02 14:22:43 -0400

    library css

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,11 @@ const LibrarySong = ({ song }) => {
     return(
         <div className="library-song">
             <img alt={song.name} src={song.cover}/>
-            <h3>{song.name}</h3>
-            <h4>{song.artist}</h4>
+            <div className="song-description">
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
+            </div>
+            
         </div>
     )
 }

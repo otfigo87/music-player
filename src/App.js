7f8ebfb50dd94eb73cd4639cd65d0e620c82ff19
otmane aatik <aaotmane@gmail.com>
2021-06-03 11:47:26 -0400

    selecting songs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           currentSong={currentSong} 
           isPlaying={isPlaying} 
           setIsPlaying={setIsPlaying} />
-          <Library songs={songs}/>
+          <Library songs={songs} setCurrentSong={setCurrentSong}/>
     </div>
   );
 }
